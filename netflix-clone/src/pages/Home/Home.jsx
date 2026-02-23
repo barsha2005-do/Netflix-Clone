5abca612b0ba55ba91fc37c82fc2f868744c49eb
barsha2005-do <barshap674@gmail.com>
2026-02-24 00:58:27 +0530

    Add hero section with caption and buttons to Home component

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
     return (
@@ -13,6 +15,10 @@ const Home = () => {
                 <div className="hero-caption">
                     <img src={hero_title} alt="" className='caption-img' />
                     <p>Discovering his ties to a secret ancent order, a young man living in mordern istanbul embarks on a quest to save the city from an immortal enemy.</p>
+                    <div className="hero-btns">
+                        <button className='btn'><img src={play_icon} alt="" />Play</button>
+                        <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+                    </div>
                 </div>
             </div>
         </div>
