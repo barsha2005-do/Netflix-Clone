f92327240092030bcac023967f6745bb5ceb21a3
barsha2005-do <barshap674@gmail.com>
2026-02-26 00:45:35 +0530

    Add TitleCards component to Home page and style adjustments

--- netflix-clone/src/components/TitleCrads/TitleCards.jsx
+++ netflix-clone/src/components/TitleCrads/TitleCards.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './TitleCards.css'
 import Cards_data from '../../assets/cards/Cards_data'
-
 const TitleCards = () => {
     return (
-        <div className='titlecards'>TitleCards</div>
+        <div className='titlecards'>
+            <h2>Popular on Netflix</h2>
+            <div className="card-list">
+                {Cards_data.map((card, index) => { return <div className='card' key={index}><img src={card.image} alt="" /><p>{card.name}</p></div> })}
+            </div>
+        </div>
     )
 }
 
